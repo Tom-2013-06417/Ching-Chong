b7a08f8bd5f209bd1f7e113e57b2414e958c27ce
Simon Dostoevsky <diego_1297@yahoo.com>
2015-05-28 22:47:10 +0800

    Sorry kevin
    we also stripped........ the lines para matanggal yung tabs. pwede rin
    yata magamit yung next line functions for the ifs and loops. wala lang.
    mahal ka namin kevin -marko

--- Parser/interpreter.py
+++ Parser/interpreter.py
@@ -1,7 +1,20 @@
 
+def nextline(file, num):
+	num = num + 1
+	for num, line in enumerate(file, num):
+		line = line.strip()
+		if line == "I did not know lah.":
+			return
+		line = line.split(" lah")[0]
+		print line
+		
+
+
 def interpreter(list):
-	file = open("SampleCode.chng","r")
+	ArithFlag = False
+	file = open("mySecondishProgram.chng","r")
 	for num, line in enumerate(file, 1):					#line is literally just what the string in the line is, num is what line number
+		line = line.strip()
 		if num == list[1]:									#given our list from parser, we check if our current line number is what we want to parse
 			if list[0] == "Print":							#conditions to what to translate chingchong into
 				printList = line.split("I show Father")		#take away what we dont want
@@ -20,5 +33,23 @@ def interpreter(list):
 				variable = inputList2[1].split(" lah")[0]
 				writeString = str(variable) + "= input(" + prompt + ")"
 				print writeString
-list = ["Input",16]
+			if list[0] == "Arithmetic":
+				inputList = line.split("Father surprise quiz: ")
+				inputList2 = inputList[1].split(" lah")
+				arith = inputList2[0]
+				print arith
+			if list[0] == "ArithmeticBlock":
+				nextline(file, num)
+			if list[0] == "Break":
+				breaker = "break"
+				print breaker
+			if list[0] == "Continue":
+				continuer = "continue"
+				print continuer
+			if list[0] == "For":
+				pass
+
+			break
+
+list = ["ArithmeticBlock", 141]
 interpreter(list)
