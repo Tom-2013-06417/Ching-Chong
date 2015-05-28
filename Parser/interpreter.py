d5d6274467c3d30639cc78b722bd09dab9d49b5b
Kevin F <kozlozkof@gmail.com>
2015-05-29 00:56:14 +0800

    merged...?

--- Parser/interpreter.py
+++ Parser/interpreter.py
@@ -14,33 +14,40 @@ def nextline(file, num):
 		
 		string = string + line + "\n"
 		
+def getEnder(file, num, label=0):
+	num = num + 1
+	loopend = "I'm done with " + str(label) + " lah."
+	for num, line in enumerate(file, num):
+		line = line.strip()
+		if line == loopend:
+			return num
+
+
+def interpreter(mainlist, n, indents=0):
 	
-
-
-def interpreter(list):
-	ArithFlag = False
 	file = open("mySecondishProgram.chng","r")
+	writervar = '\t'*indents
 	for num, line in enumerate(file, 1):					#line is literally just what the string in the line is, num is what line number
 		line = line.strip()
 		if num == list[1]:									#given our list from parser, we check if our current line number is what we want to parse
 			if list[0] == "Print":							#conditions to what to translate chingchong into
 				printList = line.split("I show Father")		#take away what we dont want
 				printList = printList[1].split(" lah")		#take away the other side
-				writeString = "print " + printList[0]		#writeString is what we want to write to the python file
+				writeString = writervar + "print " + printList[0]		#writeString is what we want to write to the python file
 				print writeString
 			if list[0] == "Input":
 				inputList = line.split("I give Father ")
 				variable = inputList[1].split(" lah")[0]
-				writeString = str(variable) + "= input()"
+				writeString = writervar + str(variable) + "= input()"
 				print writeString
 			if list[0] == "InputPrompt":
 				inputList = line.split("Father wants ")
 				inputList2 = inputList[1].split(", I give Father ")
 				prompt = inputList2[0]
 				variable = inputList2[1].split(" lah")[0]
-				writeString = str(variable) + "= input(" + prompt + ")"
+				writeString = writervar + str(variable) + "= input(" + prompt + ")"
 				print writeString
-			if list[0] == "VarDecSegment":
+			if list[0] == "VarDecSegment" || list[0] == "GlobalVarDec" :
 				variablesDeclared = nextline(file, num)
 				listedVars = variablesDeclared.split("\n")
 				variableDict = {"Int":[],"Float":[],"Bool":[],"Char":[],"String":[],"List":[]}
@@ -89,21 +96,44 @@ def interpreter(list):
 				inputList = line.split("Father surprise quiz: ")
 				inputList2 = inputList[1].split(" lah")
 				arith = inputList2[0]
-				writeString = arith
+				writeString = writervar + arith
 				print writeString
 			if list[0] == "ArithmeticBlock":
-				writeString = nextline(file, num)
+				writeString = writervar + nextline(file, num)
 				print writeString
 			if list[0] == "Break":
 				breaker = "break"
-				writeString = breaker
+				writeString = writervar + breaker
 				print writeString
 			if list[0] == "Continue":
 				continuer = "continue"
-				writeString = continuer
+				writeString = writervar +  continuer
 				print writeString
 			if list[0] == "For":
-				pass
+				forList = line.split("Father say make repeat ")
+				forList2 = forList[1].split(" again lah.")
+				label = forList2[0]
+				num = num + 1
+				for num, newline in enumerate(file, num):
+					condline = newline
+					break
+
+				condvar = condline.split(" from ")[0].split("Must do ")[1]
+				condrng1 = condline.split(" from ")[1].split(" to ")[0]
+				condrng2 = condline.split(" from ")[1].split(" to ")[1].split(" oclock")[0]
+
+				writervar = writervar + "for " + condvar + " in range(" + condrng1 + "," + condrng2 + "):"
+				print writervar
+
+				limit = getEnder(file, num, label)
+				n = n + 1
+				try:
+					while mainlist[n][1] < limit:
+						indents += 1
+						interpreter(mainlist, n, indents) 
+						n = n + 1
+				except IndexError:
+					pass
 
 			break
 
