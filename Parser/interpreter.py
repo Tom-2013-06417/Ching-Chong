f3788ea88e62a95dfe1c4ab9c115ca075d4f0083
Simon Dostoevsky <diego_1297@yahoo.com>
2015-05-29 01:09:53 +0800

    FINALLY MERGE HAPPENED TO ME

--- Parser/interpreter.py
+++ Parser/interpreter.py
@@ -1,19 +1,21 @@
 
-def nextline(file, num):
+def nextline(file, num, indents=0):
 	num = num + 1
 	string = ""
 	for num, line in enumerate(file, num):
 		line = line.strip()
 		if line == "I did not know lah.":
 			return string
-		if line =="to become doctor lah.":
+		elif line =="to become doctor lah.":
 			return string
-		if "Sincerely, " in line:
+		elif "Sincerely, " in line:
 			return string
-		line = line.split(" lah")[0]
-		
-		string = string + line + "\n"
-		
+		else:
+			line = '\t'*indents + line.split(" lah")[0]
+			string = string + line + "\n"
+
+
+
 def getEnder(file, num, label=0):
 	num = num + 1
 	loopend = "I'm done with " + str(label) + " lah."
@@ -23,31 +25,47 @@ def getEnder(file, num, label=0):
 			return num
 
 
+
 def interpreter(mainlist, n, indents=0):
-	
+	list = mainlist[n]
 	file = open("mySecondishProgram.chng","r")
 	writervar = '\t'*indents
+
 	for num, line in enumerate(file, 1):					#line is literally just what the string in the line is, num is what line number
 		line = line.strip()
 		if num == list[1]:									#given our list from parser, we check if our current line number is what we want to parse
 			if list[0] == "Print":							#conditions to what to translate chingchong into
 				printList = line.split("I show Father")		#take away what we dont want
 				printList = printList[1].split(" lah")		#take away the other side
-				writeString = writervar + "print " + printList[0]		#writeString is what we want to write to the python file
-				print writeString
+				writervar = writervar + "print" + printList[0]		#writervar is what we want to write to the python file
+				print writervar
 			if list[0] == "Input":
 				inputList = line.split("I give Father ")
 				variable = inputList[1].split(" lah")[0]
-				writeString = writervar + str(variable) + "= input()"
-				print writeString
+				writervar = writervar + str(variable) + "= input()"
+				print writervar
 			if list[0] == "InputPrompt":
 				inputList = line.split("Father wants ")
 				inputList2 = inputList[1].split(", I give Father ")
 				prompt = inputList2[0]
 				variable = inputList2[1].split(" lah")[0]
-				writeString = writervar + str(variable) + "= input(" + prompt + ")"
-				print writeString
-			if list[0] == "VarDecSegment" || list[0] == "GlobalVarDec" :
+				writervar = writervar + str(variable) + "= input(" + prompt + ")"
+				print writervar
+			if list[0] == "Arithmetic":
+				inputList = line.split("Father surprise quiz: ")
+				inputList2 = inputList[1].split(" lah")
+				writervar = writervar + inputList2[0]
+				print writervar
+			if list[0] == "ArithmeticBlock":
+				writervar = nextline(file, num, indents)
+				print writervar
+			if list[0] == "Break":
+				writervar = writervar + "break"
+				print writervar
+			if list[0] == "Continue":
+				writervar = writervar + "continue"
+				print writervar
+			if list[0] == "VarDecSegment":
 				variablesDeclared = nextline(file, num)
 				listedVars = variablesDeclared.split("\n")
 				variableDict = {"Int":[],"Float":[],"Bool":[],"Char":[],"String":[],"List":[]}
@@ -92,23 +110,6 @@ def interpreter(mainlist, n, indents=0):
 					
 
 				print variableDict
-			if list[0] == "Arithmetic":
-				inputList = line.split("Father surprise quiz: ")
-				inputList2 = inputList[1].split(" lah")
-				arith = inputList2[0]
-				writeString = writervar + arith
-				print writeString
-			if list[0] == "ArithmeticBlock":
-				writeString = writervar + nextline(file, num)
-				print writeString
-			if list[0] == "Break":
-				breaker = "break"
-				writeString = writervar + breaker
-				print writeString
-			if list[0] == "Continue":
-				continuer = "continue"
-				writeString = writervar +  continuer
-				print writeString
 			if list[0] == "For":
 				forList = line.split("Father say make repeat ")
 				forList2 = forList[1].split(" again lah.")
@@ -134,8 +135,8 @@ def interpreter(mainlist, n, indents=0):
 						n = n + 1
 				except IndexError:
 					pass
-
 			break
 
-list = ["VarDecSegment", 5]
-interpreter(list)
+list = [["ArithmeticBlock", 141]]
+#list = [["For", 92], ["Print", 94]] 
+interpreter(list, 0)
